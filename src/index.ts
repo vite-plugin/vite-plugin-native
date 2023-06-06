1f0c43f993de00a0255349d58be2c14e5966ed1b
草鞋没号 <308487730@qq.com>
2023-06-06 11:25:38 +0800

    refactor(0.1.0-beta.1): re-design API

--- src/index.ts
+++ src/index.ts
@@ -3,53 +3,67 @@ import path from 'node:path'
 import type { PluginContext } from 'rollup'
 import {
   type Plugin,
+  type ResolvedConfig,
   normalizePath,
 } from 'vite'
 import {
   MagicString,
+  cleanUrl,
   node_modules as find_node_modules,
+  relativeify,
 } from 'vite-plugin-utils/function'
 
 export interface NativeOptions {
-  /** Where we want to physically put the extracted .node files */
-  copyTo?: string
-  /** Path to the same folder, relative to the output bundle js */
-  destDir?: string
   /**
-   * - Use `dlopen` instead of `require`/`import`.
-   * - This must be set to true if using a different file extension that '.node'
+   * Where we want to physically put the extracted `.node` files
+   * @default 'dist-native'
    */
-  dlopen?: boolean
+  outDir?: string
   /**
    * - Modify the final filename for specific modules
    * - A function that receives a full path to the original file, and returns a desired filename
    * - Or a function that returns a desired file name and a specific destination to copy to
+   * @experimental
+   * @todo better calculation value of `id` automatically
    */
-  map?: (modulePath: string) => string | { name: string; copyTo: string }
-  /** If the target is ESM, so we can't use `require` (and .node is not supported in `import` anyway), we will need to use `createRequire` instead. */
-  target?: 'esm' | 'cjs'
+  map?: (mapping: {
+    /** `.node` file path */
+    native: string
+    /** require id of `.node` file */
+    id: string
+    /** `.node` file output location */
+    output: string
+  }) => { id: string; output: string }
+  /**
+   * - Use `dlopen` instead of `require`/`import`
+   * - This must be set to true if using a different file extension that `.node`
+   */
+  dlopen?: boolean
+  /**
+   * If the target is `esm`, so we can't use `require` (and `.node` is not supported in `import` anyway), we will need to use `createRequire` instead 
+   * @default 'cjs'
+   */
+  target?: 'cjs' | 'esm'
+  /** For cross-compile */
   platform?: typeof process.platform
+  /** For cross-compile */
   arch?: typeof process.arch
 }
 
 const TAG = '[vite-plugin-native]'
 const PREFIX = '\0native:'
+let config: ResolvedConfig
 const opts: Required<NativeOptions> = {
-  copyTo: 'dist/native',
-  destDir: './',
+  outDir: 'dist-native',
+  map: mapping => mapping,
   dlopen: false,
-  map: modulePath => generateDefaultMapping(modulePath),
   target: 'cjs',
   platform: process.platform,
   arch: process.arch
 }
-const moduleCache = new Map<string, { name: string; copyTo: string; }>()
+const moduleCache = new Map<string, { id: string; output: string; }>()
 
 export default function native(options: NativeOptions = {}): Plugin[] {
-  Object.assign(opts, options)
-  opts.copyTo = normalizePath(opts.copyTo)
-  opts.destDir = normalizePath(opts.destDir)
-
   return [
     {
       name: 'vite-plugin-native:resolve',
@@ -72,35 +86,40 @@ export default function native(options: NativeOptions = {}): Plugin[] {
     },
     {
       name: 'vite-plugin-native',
-      config() {
-        if (fs.existsSync(opts.copyTo)) {
-          fs.mkdirSync(opts.copyTo, { recursive: true })
-        }
+      configResolved(_config) {
+        config = _config
+
+        // NativeOptions initialize
+        Object.assign(opts, options)
+        opts.outDir = path.posix.resolve(_config.root, normalizePath(opts.outDir))
+
+        fs.mkdirSync(opts.outDir, { recursive: true })
       },
       load(id) {
         if (id.startsWith(PREFIX)) {
           return exportModule(id.slice(PREFIX.length))
         }
 
-        // 🤔 never
-        //´👉 https://github.com/danielgindi/rollup-plugin-natives/blob/0.7.6/src/index.js#L155
-        // const module = moduleCache.get(id)
-        // if (module) {
-        //   return exportModule(module.name)
-        // }
+        const module = moduleCache.get(id)
+        if (module) {
+          return exportModule(module.id)
+        }
       },
       transform(code, id) {
         const ms = new MagicString(code)
         const bindingsRgx = /require\(['"]bindings['"]\)\(((['"]).+?\2)?\)/g
         const simpleRequireRgx = /require\(['"](.*?)['"]\)/g // TODO: use AST parser
-        const node_modules = find_node_modules(id)[0]
+        const clean_id = cleanUrl(id.startsWith('\0') ? id.replace('\0', '') : id)
+        const node_modules = find_node_modules(clean_id)[0]
+
+        if (!node_modules) return
 
         const hasBindingReplacements = replace(
           code,
           ms,
           bindingsRgx,
           match => {
-            const name = match[1]
+            const [, name] = match
 
             let nativeAlias: string = name ? new Function('return ' + name)() : 'bindings.node';
             if (!nativeAlias.endsWith('.node'))
@@ -145,7 +164,7 @@ export default function native(options: NativeOptions = {}): Plugin[] {
           ms,
           simpleRequireRgx,
           match => {
-            let name = match[1]
+            let [, name] = match
 
             if (!name.endsWith('.node'))
               name += '.node'
@@ -163,10 +182,10 @@ export default function native(options: NativeOptions = {}): Plugin[] {
 
         let hasBinaryReplacements = false
         if (code.includes('node-pre-gyp')) {
-          const varRgx = /(var|let|const)\s+([a-zA-Z0-9_]+)\s+=\s+require\((['"])(@mapbox\/node-pre-gyp|node-pre-gyp)\3\);?/g
-          const varMatch = varRgx.exec(code)
-          const binaryRgx = varMatch
-            ? new RegExp(`\\b(var|let|const)\\s+([a-zA-Z0-9_]+)\\s+=\\s+${varMatch[2]}\\.find\\(path\\.resolve\\(path\\.join\\(__dirname,\\s*((?:['"]).*\\4)\\)\\)\\);?\\s*(var|let|const)\\s+([a-zA-Z0-9_]+)\\s+=\\s+require\\(\\2\\)`, 'g')
+          const node_pre_gyp_Rgx = /(var|let|const)\s+([a-zA-Z0-9_]+)\s+=\s+require\((['"])(@mapbox\/node-pre-gyp|node-pre-gyp)\3\);?/g
+          const node_pre_gyp_Match = node_pre_gyp_Rgx.exec(code)
+          const binaryRgx = node_pre_gyp_Match
+            ? new RegExp(`\\b(var|let|const)\\s+([a-zA-Z0-9_]+)\\s+=\\s+${node_pre_gyp_Match[2]}\\.find\\(path\\.resolve\\(path\\.join\\(__dirname,\\s*((?:['"]).*\\4)\\)\\)\\);?\\s*(var|let|const)\\s+([a-zA-Z0-9_]+)\\s+=\\s+require\\(\\2\\)`, 'g')
             : /\b(var|let|const)\s+([a-zA-Z0-9_]+)\s+=\s+binary\.find\(path\.resolve\(path\.join\(__dirname,\s*((?:['"]).*\4)\)\)\);?\s*(var|let|const)\s+([a-zA-Z0-9_]+)\s+=\s+require\(\2\)/g
 
           hasBinaryReplacements = replace(
@@ -174,40 +193,40 @@ export default function native(options: NativeOptions = {}): Plugin[] {
             ms,
             binaryRgx,
             match => {
-              let preGyp = null
-
-              const r1 = varMatch && varMatch[4][0] === '@' ? '@mapbox/node-pre-gyp' : 'node-pre-gyp'
-              const r2 = varMatch && varMatch[4][0] === '@' ? 'node-pre-gyp' : '@mapbox/node-pre-gyp'
+              let node_pre_gyp: any
 
               // We can't simply require('node-pre-gyp') because we are not in the same context as the target module
               // Maybe node-pre-gyp is installed in node_modules/target_module/node_modules
-              let preGypPath = path.dirname(id)
-              while (preGypPath !== '/' && preGyp == null) {
+              let node_pre_gyp_path = path.dirname(clean_id)
+              let node_pre_gyp_path_pre: string | undefined
+              while (node_pre_gyp_path !== node_pre_gyp_path_pre) {
                 // Start with the target module context and then go back in the directory tree
                 // until the right context has been found
                 try {
                   // noinspection NpmUsedModulesInstalled
-                  preGyp = require(path.resolve(path.join(preGypPath, 'node_modules', r1)))
-                } catch (ex) {
-                  try {
-                    // noinspection NpmUsedModulesInstalled
-                    preGyp = require(path.resolve(path.join(preGypPath, 'node_modules', r2)))
-                  } catch (ex) {
-                    // ignore
-                  }
-                }
-                preGypPath = path.dirname(preGypPath)
+                  node_pre_gyp ??= require(path.resolve(path.join(node_pre_gyp_path, 'node_modules', '@mapbox/node-pre-gyp')))
+                } catch { }
+                try {
+                  // noinspection NpmUsedModulesInstalled
+                  node_pre_gyp ??= require(path.resolve(path.join(node_pre_gyp_path, 'node_modules', 'node-pre-gyp')))
+                } catch { }
+
+                if (node_pre_gyp) break
+
+                // lookup
+                node_pre_gyp_path_pre = node_pre_gyp_path
+                node_pre_gyp_path = path.dirname(node_pre_gyp_path)
               }
 
-              if (!preGyp) return
+              if (!node_pre_gyp) return
 
               let [, d1, v1, ref, d2, v2] = match
 
-              const libPath = preGyp.find(path.resolve(path.join(path.dirname(id), new Function('return ' + ref)())), options)
+              const libPath = node_pre_gyp.find(path.resolve(path.join(path.dirname(clean_id), new Function('return ' + ref)())), options)
 
               let prefixedId = mapAndReturnPrefixedId.apply(this, [libPath])
               if (prefixedId) {
-                return `${d1} ${v1}=${JSON.stringify(moduleCache.get(libPath)!.name.replace(/\\/g, '/'))};${d2} ${v2}=require(${JSON.stringify(prefixedId)})`
+                return `${d1} ${v1}=${JSON.stringify(moduleCache.get(libPath)!.id.replace(/\\/g, '/'))};${d2} ${v2}=require(${JSON.stringify(prefixedId)})`
               }
             },
           )
@@ -217,7 +236,7 @@ export default function native(options: NativeOptions = {}): Plugin[] {
           // pulls some additional dependencies - like AWS S3 - which are needed only for downloading
           // new binaries
           if (hasBinaryReplacements)
-            replace(code, ms, varRgx, () => '')
+            replace(code, ms, node_pre_gyp_Rgx, () => '')
         }
 
         if (![
@@ -259,21 +278,6 @@ export default require(${JSON.stringify(id)});
   return `export default require(${JSON.stringify(id)});\n`
 }
 
-function rebaseModule(basename: string) {
-  return (opts.destDir + (/\\$|\/$/.test(opts.destDir) ? '' : '/') + basename).replace(/\\/g, '/')
-}
-
-function findAvailableBasename(nativePath: string) {
-  let basename = path.basename(nativePath)
-
-  let i = 1
-  while (Array.from(moduleCache.values()).filter(x => x.name === rebaseModule(basename)).length) {
-    basename = path.basename(nativePath, path.extname(nativePath)) + '_' + (i++) + path.extname(nativePath)
-  }
-
-  return basename
-}
-
 function replace(
   code: string,
   ms: MagicString,
@@ -297,43 +301,44 @@ function replace(
   return false
 }
 
-function generateDefaultMapping(nativePath: string) {
-  const basename = findAvailableBasename(nativePath)
-
+function generateDefaultMapping(native: string): ReturnType<NonNullable<NativeOptions['map']>> {
+  const basename = path.basename(native)
+  // If the user output the file name with a dir, then `relativePath` based on `outDir` will no longer be correct.
+  // e.g. - `js/[name].js`
+  // TODO: better calculation value of `id` automatically
+  const outDir = path.posix.resolve(config.root, config.build.outDir)
+  const relativePath = relativeify(path.posix.relative(outDir, opts.outDir))
   return {
-    name: rebaseModule(basename),
-    copyTo: path.posix.join(opts.copyTo, basename),
+    id: path.posix.join(relativePath, basename),
+    output: path.join(opts.outDir, basename),
   }
 }
 
 function mapAndReturnPrefixedId(this: PluginContext, importee: string, importer?: string) {
   const resolvedPath = path.posix.resolve(importer ? path.dirname(importer) : '', importee)
 
-  let nativePath: string | undefined
+  let native: string | undefined
   if (/\.(node|dll)$/i.test(importee))
-    nativePath = resolvedPath
+    native = resolvedPath
   else if (fs.existsSync(resolvedPath + '.node'))
-    nativePath = resolvedPath + '.node'
+    native = resolvedPath + '.node'
   else if (fs.existsSync(resolvedPath + '.dll'))
-    nativePath = resolvedPath + '.dll'
+    native = resolvedPath + '.dll'
 
-  if (nativePath) {
-    let module = moduleCache.get(nativePath)
+  if (native) {
+    let module = moduleCache.get(native)
 
     if (!module) {
-      const mapping = opts.map(nativePath)
-      module = typeof mapping === 'string'
-        ? generateDefaultMapping(mapping)
-        : mapping
-      moduleCache.set(nativePath, module)
+      const mapping = generateDefaultMapping(native)
+      moduleCache.set(native, module = opts.map({ native, ...mapping }) ?? mapping)
 
-      if (fs.existsSync(nativePath)) {
-        fs.copyFileSync(nativePath, module.copyTo)
+      if (fs.existsSync(native)) {
+        fs.copyFileSync(native, module.output)
       } else {
-        this.warn(`${TAG} ${nativePath} does not exist`)
+        this.warn(`${TAG} ${native} does not exist`)
       }
     }
 
-    return PREFIX + module.name
+    return PREFIX + module.id
   }
 }
